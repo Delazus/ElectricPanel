eb59059bb311092992ce467ecb68d3346f02e29d
Delazus <48772508+Delazus@users.noreply.github.com>
2025-07-13 22:56:47 -0500

    v2

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function showInfo(index) {
   const text = document.getElementById("tooltip-text");
 
   if (index === 1) {
-    text.textContent = "Este es el taco principal. Usado para cortar todo el suministro.";
+    text.textContent = "Este es el taco principal. Corta todo el suministro.";
   } else if (index === 2) {
-    text.textContent = "Este controla la línea de cocina. Úsalo si hay sobrecarga.";
+    text.textContent = "Este controla la cocina. Úsalo si hay sobrecarga.";
   }
 
   tooltip.classList.remove("hidden");
